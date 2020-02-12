a81063550f1a41319052749c2d64576cc2ba6fd3
xxx-kohe-xxx <kohe.dekita@gmail.com>
2020-02-12 23:46:19 +0900

    商品カードコンポーネント作成

---
+++ client/pages/select/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <ProductCard
+      v-for="product in products"
+      v-bind:key="product.id"
+      v-bind:name="product.name"
+      v-bind:price="product.price"
+    />
+  </div>
+</template>
+
+<script>
+import ProductCard from '~/components/ProductCard.vue'
+export default {
+  components: {
+    ProductCard
+  },
+  data () {
+    return {
+      products: [
+        { id: 1, name: 'まぐろ', price: 100 },
+        { id: 2, name: 'ローストビーフ カルパッチョ', price: 100 },
+        { id: 3, name: 'まぐろはらみ', price: 100 }
+      ]
+    }
+  }
+}
+</script>
