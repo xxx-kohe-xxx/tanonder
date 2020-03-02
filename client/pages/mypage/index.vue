244aa214645d800723faca84ee9ace45aafad25f
xxx-kohe-xxx <kohe.dekita@gmail.com>
2020-03-03 08:36:09 +0900

    マイページ画面作成 スタイル設定

---
+++ client/pages/mypage/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="p-mypage">
+    <h1 class="p-mypage__heading">
+      マイページ
+    </h1>
+    <OrderList class="p-mypage__order-list" />
+    <button type="submit" class="p-mypage__button">
+      変更
+    </button>
+  </div>
+</template>
+
+<script>
+import OrderList from '~/components/OrderList'
+
+export default {
+  components: {
+    OrderList
+  }
+}
+</script>
