cfa8d02e725dfd02b6de921dc27d798a23b27db9
xxx-kohe-xxx <kohe.dekita@gmail.com>
2020-03-03 19:44:57 +0900

    一覧表示コンポーネント スタイル設定

---
+++ client/components/OrderList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="p-order-list">
+    <ul class="p-order-list__header">
+      <li class="p-order-list__header-name">
+        品名
+      </li>
+      <li class="p-order-list__header-quantity">
+        個数
+      </li>
+      <li class="p-order-list__header-price">
+        金額
+      </li>
+    </ul>
+    <ul class="p-order-list__product">
+      <li class="p-order-list__product-name">
+        まぐろ
+      </li>
+      <li class="p-order-list__product-quantity">
+        1
+      </li>
+      <li class="p-order-list__product-price">
+        108
+      </li>
+    </ul>
+    <ul class="p-order-list__total">
+      <li class="p-order-list__total-name">
+        合計
+      </li>
+      <li class="p-order-list__total-quantity">
+        {{ quantity }}
+      </li>
+      <li class="p-order-list__total-price">
+        {{ price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
