1b8082f1d8956d7a28e49c996363d7a471b17ae8
xxx-kohe-xxx <kohe.dekita@gmail.com>
2020-03-04 20:01:41 +0900

    menu page 画面作成

---
+++ client/components/menu.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="p-menu">
+    <h1 class="p-menu__heading">
+      MENU
+    </h1>
+    <div class="p-menu__link-section">
+      <nuxt-link class="p-menu__link-item">
+        マイページ
+      </nuxt-link>
+      <nuxt-link class="p-menu__link-item">
+        みんなが頼んだ商品
+      </nuxt-link>
+      <nuxt-link class="p-menu__link-item">
+        商品を頼み直す
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
